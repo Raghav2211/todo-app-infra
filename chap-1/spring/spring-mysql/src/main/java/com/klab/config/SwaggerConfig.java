685e7095e2e88e93df0ff6d6d9b5ebe51d242e56
Raghav2211 <rghvj.22@gmail.com>
2020-11-13 11:45:15 +0000

    Add swagger api info

--- chap-1/spring/spring-mysql/src/main/java/com/klab/config/SwaggerConfig.java
+++ chap-1/spring/spring-mysql/src/main/java/com/klab/config/SwaggerConfig.java
@@ -3,22 +3,36 @@
  */
 package com.klab.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfig {                                    
+public class SwaggerConfig {
+
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())           
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select().apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build().apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        return new ApiInfo("TODO App on Kubernetes application", "TODO App on Kubernetes application", "0.1.0",
+                "Terms of service",
+                new Contact("TODO",
+                        "https://github.com/Raghav2211/kubernetes-lab/tree/spring-mysql/chap-1/spring/spring-mysql",
+                        "todo@todo.com"),
+                "TODO Licence", "http://localhost:8080", List.of());
+    }
+
 }
