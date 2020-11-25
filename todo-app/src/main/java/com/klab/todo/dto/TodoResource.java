44fc6662f2ca7e1d6153d0a3b5501e5b71727a34
Ayashkant Baral <Ayashkant.Baral@rishabh.com>
2020-11-25 17:19:46 +0530

    validator for rest endpoints
    1. Added validator for rest endponts
    2. Updated Swagger docs with new error codes
    3. Create API is now updated with payload without id

---
+++ todo-app/src/main/java/com/klab/todo/dto/TodoResource.java
@@ -0,0 +1,17 @@
+package com.klab.todo.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class TodoResource {
+    @NotEmpty(message = "Content not specified")
+    private String content;
+    private Boolean isComplete = false;
+}
--- todo-app/src/main/java/com/klab/todo/exception/TodoExceptionHandler.java
+++ todo-app/src/main/java/com/klab/todo/exception/TodoExceptionHandler.java
@@ -3,15 +3,26 @@ package com.klab.todo.exception;
 import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.klab.todo.entity.Todo;
 
 @ControllerAdvice
-public class TodoExceptionHandler {
+@RestController
+public class TodoExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final String MY_SQL_UNAVAILABLE = "MySQL Unavailable..";
 
@@ -20,10 +31,18 @@ public class TodoExceptionHandler {
     public @ResponseBody TodoException handleException(final Exception exception, final HttpServletRequest request) {
         TodoException error = new TodoException();
         error.setMessage(exception.getMessage());
-        error.setRequestedURI(request.getRequestURI());
-
+        error.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error;
     }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public @ResponseBody TodoException handleDataAaccessException(final Exception exception, final HttpServletRequest request) {
+        TodoException error = new TodoException();
+        error.setMessage(exception.getMessage().replace("class "+Todo.class.getName()+ " entity", "Todo"));
+        error.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return error;
+    }
+    
 
     @ExceptionHandler(TransactionException.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
@@ -32,21 +51,43 @@ public class TodoExceptionHandler {
 
         TodoException error = new TodoException();
         error.setMessage(MY_SQL_UNAVAILABLE);
-        error.setRequestedURI(request.getRequestURI());
+        error.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
         return error;
     }
-    
+
     @ExceptionHandler(PersistenceException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public @ResponseBody TodoException handlePersistence(final Exception exception,
-            final HttpServletRequest request) {
-        
+    public @ResponseBody TodoException handlePersistence(final Exception exception, final HttpServletRequest request) {
+
         TodoException error = new TodoException();
         error.setMessage(exception.getMessage());
-        error.setRequestedURI(request.getRequestURI());
+        error.setCode(HttpStatus.NOT_FOUND.value());
 
         return error;
     }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
+            WebRequest request) {
+        var bindingResult = ex.getBindingResult();
+        var fieldErrors = bindingResult.getFieldErrors();
+        TodoException error = new TodoException();
+        error.setMessage(fieldErrors.get(0).getDefaultMessage().contains("null") ? "Tood id must not be null"
+                : fieldErrors.get(0).getDefaultMessage());
+        error.setCode(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        TodoException error = new TodoException();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setCode(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    
+    
 
 }
