da6ad4269dfc1af474bc36fe7d7289c30fd63300
Raghav2211 <rghvj.22@gmail.com>
2020-12-13 12:02:02 +0000

    install docker-machine(win/linux/OSX) if not exists

--- todo-app/swarm/cluster.sh
+++ todo-app/swarm/cluster.sh
@@ -2,6 +2,21 @@
 
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+        chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine; }
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
+  		chmod +x /usr/local/bin/docker-machine; }
+elif [[ "$OSTYPE" == "msys" ]]; then
+        docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; if [[ ! -d "$HOME/bin" ]]; then mkdir -p "$HOME/bin"; fi && \
+		curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-Windows-x86_64.exe > "$HOME/bin/docker-machine.exe" && \
+		chmod +x "$HOME/bin/docker-machine.exe"; }
+else
+        echo "Unknown OS"
+        exit 1;
+fi
+
 function create_cluster() {
 
 	# Input manager & worker nodes
@@ -84,7 +99,7 @@ function delete_cluster() {
 }
 
 function help() {
-    echo "Usage:    ${me} [-hcd]  [-h|--help help] [-c|--create Create a new swarm cluster] [-d|--delete Delete new swarm cluster] [-v|--view View cluster info/health]"
+    echo "Usage:    ${me} [-hcd]  [-h|--help help] [-c|--create Create a new swarm cluster] [-d|--delete Delete swarm cluster] [-v|--view View cluster info/health]"
 }
 if [ $# -eq 0 ]
 then
