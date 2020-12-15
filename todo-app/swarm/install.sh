ce4102f6475ce9c65fc579f93e35b3ac13006089
Raghav2211 <rghvj.22@gmail.com>
2020-12-15 12:26:37 +0000

    install virtualbox for darwin

--- todo-app/swarm/install.sh
+++ todo-app/swarm/install.sh
@@ -3,8 +3,20 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
         chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine; }
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+		brew --version &> /dev/null || { echo "Installing brew....."; curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh; }
         docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
   		chmod +x /usr/local/bin/docker-machine; }
+  		vboxmanage --version &> /dev/null && { 
+  			read -p "Do you wish to install virtualbox[Y/n]?" in; 
+  			case ${in:0:1} in
+			    y|Y )
+			        brew cask install virtualbox
+			    ;;
+			    n/N )
+			        echo "VirtualBox not installed"
+			    ;;
+			esac
+  		}
 elif [[ "$OSTYPE" == "msys" ]]; then
         docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; if [[ ! -d "$HOME/bin" ]]; then mkdir -p "$HOME/bin"; fi && \
 		curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-Windows-x86_64.exe > "$HOME/bin/docker-machine.exe" && \
