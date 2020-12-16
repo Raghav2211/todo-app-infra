2ce7bf80cc54df00b71799756784ec002f387e1d
Raghav2211 <rghvj.22@gmail.com>
2020-12-16 07:02:45 +0000

    1. Remove unused code 2. curl with no o/p

--- todo-app/swarm/install-dependencies.sh
+++ todo-app/swarm/install-dependencies.sh
@@ -1,68 +1,10 @@
 # #!/bin/bash
-# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#         docker-machine --version &> /dev/null || { echo "Installing docker-machine....."; curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-#         chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine; }
-# elif [[ "$OSTYPE" == "darwin"* ]]; then
-#   		  brew --version &> /dev/null || 
-#         { 
-#           echo "Installing brew....."; 
-#           curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh; 
-#         }
-#         docker-machine --version &> /dev/null || 
-#         { 
-#           echo "Installing docker-machine....."; 
-#           curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
-#             chmod +x /usr/local/bin/docker-machine; 
-#         }
-#     		vboxmanage --version &> /dev/null || 
-#         { 
-#     			read -p "Do you wish to install virtualbox[Y/n]?" in; 
-#     			case ${in:0:1} in
-#   			    y|Y )
-#   			        brew cask install virtualbox
-#   			    ;;
-#   			    n/N )
-#   			        echo "VirtualBox not installed"
-#   			    ;;
-#   			  esac
-#     		}
-# elif [[ "$OSTYPE" == "msys" ]]; then
-#         docker-machine --version &> /dev/null || 
-#         { 
-#           echo "Installing docker-machine....."; 
-#           if [[ ! -d "$HOME/bin" ]]; then mkdir -p "$HOME/bin"; fi && \
-# 		        curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-Windows-x86_64.exe > "$HOME/bin/docker-machine.exe" && \
-#               chmod +x "$HOME/bin/docker-machine.exe"; 
-#         }
-#     		chmod +x ChocolateyInstallNonAdmin.ps1
-#         powershell.exe -ExecutionPolicy RemoteSigned -File './ChocolateyInstallNonAdmin.ps1'
-#         vboxmanage --version &> /dev/null || 
-#         { 
-#             read -p "Do you wish to install virtualbox[Y/n]?" in; 
-#             case ${in:0:1} in
-#               y|Y )              
-#                   choco install virtualbox
-#               ;;
-#               n/N )
-#                   echo "VirtualBox not installed"
-#               ;;
-#             esac
-#         }    
-# else
-#         echo "Unknown OS"
-#         exit 1;
-# fi
-# echo "=================================================="
-# echo "Docker machine installed ---  $(docker-machine --version | awk '{split($0,a," "); print a[3]}' | sed 's/.$//')"
-# echo "VirtualBox installed     ---  $( vboxmanage --version )"
-# echo "=================================================="
 
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 check_install_brew() {
   brew --version &> /dev/null || 
   { 
-    echo "Installing brew....."; 
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh; 
   }
 }
@@ -75,8 +17,7 @@ install_choco() {
 install_docker_machine_linux() {
   docker-machine --version &> /dev/null || 
   { 
-    echo "Installing docker-machine....."; 
-    curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+    curl -L -s https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
     chmod +x /tmp/docker-machine && 
     sudo cp /tmp/docker-machine /usr/local/bin/docker-machine; 
   }
@@ -85,8 +26,7 @@ install_docker_machine_linux() {
 install_docker_machine_darwin() {
   docker-machine --version &> /dev/null || 
   { 
-    echo "Installing docker-machine....."; 
-    curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
+    curl -L -s https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
     chmod +x /usr/local/bin/docker-machine; 
   }
 }
@@ -94,9 +34,8 @@ install_docker_machine_darwin() {
 install_docker_machine_win() {
   docker-machine --version &> /dev/null || 
   { 
-    echo "Installing docker-machine....."; 
     if [[ ! -d "$HOME/bin" ]]; then mkdir -p "$HOME/bin"; fi && \
-    curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-Windows-x86_64.exe > "$HOME/bin/docker-machine.exe" && \
+    curl -L -s https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-Windows-x86_64.exe > "$HOME/bin/docker-machine.exe" && \
     chmod +x "$HOME/bin/docker-machine.exe"; 
   }
 }
@@ -107,7 +46,8 @@ install_virtualbox_darwin() {
     read -p "Do you wish to install virtualbox[Y/n]?" in; 
     case ${in:0:1} in
       y|Y )
-          brew cask install virtualbox
+          export HOMEBREW_NO_AUTO_UPDATE=1
+          brew cask --quiet install virtualbox
       ;;
       n/N )
           echo "VirtualBox not installed"
@@ -133,7 +73,7 @@ install_virtualbox_win() {
 }
 
 install_docker_machine() {
-
+  echo "Installing dokcer-machine....."
  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     install_docker_machine_linux 
  elif [[ "$OSTYPE" == "darwin"* ]]; then 
@@ -146,6 +86,7 @@ install_docker_machine() {
 }
 
 install_virtualbox() {
+  echo "Installing virtualbox....."
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "TODO"
  elif [[ "$OSTYPE" == "darwin"* ]]; then 
@@ -166,6 +107,7 @@ help() {
     echo " --help, -h                       show help"
     echo ""
     echo "Commands:"
+    echo " all                              Install all dependencies" 
     echo " virtualbox                       Install Virtualbox" 
     echo " docker-machine                   Install Docker machine" 
 }
