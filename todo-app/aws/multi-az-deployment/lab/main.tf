bc11af982eff64b16b328192b47b63476c92ae71
Raghav2211 <rghvj.22@gmail.com>
2020-12-28 12:52:30 +0000

    Remove comment & fmt

--- todo-app/aws/multi-az-deployment/lab/main.tf
+++ todo-app/aws/multi-az-deployment/lab/main.tf
@@ -8,7 +8,6 @@ module "vpc" {
   env              = "lab"
   cidr             = var.cidr
   azs              = var.azs
-  #private_subnets  = var.private_subnets
   public_subnets   = var.public_subnets
   intra_subnets    = var.intra_subnets
   database_subnets = var.database_subnets
