0d7412b076835eaf955116cd7a5d854d61377ff7
Raghav2211 <rghvj.22@gmail.com>
2020-12-29 16:04:25 +0000

    Merge into main.tf

--- todo-app/aws/multi-az-deployment/lab/main.tf
+++ todo-app/aws/multi-az-deployment/lab/main.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = var.region
 }
 
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+###############################
+#             VPC             #
+###############################
 module "vpc" {
   source           = "../modules/network"
   app_name         = var.app_name
@@ -14,3 +21,18 @@ module "vpc" {
   database_subnets = var.database_subnets
 }
 
+###############################
+#       Security Groups       #
+###############################
+module "bastion_ssh" {
+  source      = "../modules/sg"
+  app_name    = var.app_name
+  app_version = var.app_version
+  env         = var.env
+  # add bastion suffix
+  #name                = "${var.app_name}-${var.env}-bastion"
+  vpc_id       = module.vpc.vpc_id
+  description  = "Bastion host security group"
+  ingress_cidr = concat(var.sg_bastion_cidrs, ["${chomp(data.http.myip.body)}/32"])
+
+}
