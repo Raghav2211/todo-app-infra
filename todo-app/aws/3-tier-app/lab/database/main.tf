d994b886ff952b07a5cd861cbe1f999fb7d685e4
Raghav2211 <rghvj.22@gmail.com>
2021-01-07 17:18:59 +0000

    Write error

--- todo-app/aws/3-tier-app/lab/database/main.tf
+++ todo-app/aws/3-tier-app/lab/database/main.tf
@@ -6,7 +6,7 @@ module "mysql" {
   app             = var.app
   instance_type   = var.instance_type
   master_user     = var.master_user
-  master_password = var.master_password
+  master_password   = var.master_password
   multi_az        = var.multi_az
 
 
