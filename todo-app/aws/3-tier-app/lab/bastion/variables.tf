b099955948f8ffd664cd0e512fc12539e94bc65d
Ayashkant Baral <Ayashkant.Baral@rishabh.com>
2021-01-08 20:58:56 +0530

    bstion module separated
    bstion module separated

--- todo-app/aws/3-tier-app/lab/bastion/variables.tf
+++ todo-app/aws/3-tier-app/lab/bastion/variables.tf
@@ -1,37 +1,4 @@
 variable "region" {}
-
-variable "app" {
-  type = object(
-    {
-      id      = string
-      version = string
-      env     = string
-    }
-  )
-  default = {
-    id      = "psi"
-    version = "1.0.0"
-    env     = "lab"
-  }
-}
-
-variable "ami" {
-  type        = string
-  description = "The AMI to use for the instance. By default it is the AMI provided by Amazon with Ubuntu 20.04"
-  default     = ""
-  # validation {
-  #   condition     = can(regex("^ami-", var.ami))
-  #   error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
-  # }
-}
-
-variable "instance_type" {
-  type        = string
-  description = "Type of instance, Default is t2.micro"
-  default     = "t2.micro"
-}
+variable "app" {}
 # Validation on admin as user
-variable "ssh_users" {
-  type        = list(map(string))
-  description = "Bastion host ssh user details"
-}
+variable "ssh_users" {}
