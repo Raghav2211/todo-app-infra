c33599e76cacece6c83d258a2b5100aa1496b634
Raghav2211 <rghvj.22@gmail.com>
2021-01-13 11:33:23 +0000

    remove ec2 association

--- todo-app/aws/3-tier-app/modules/app-server/main.tf
+++ todo-app/aws/3-tier-app/modules/app-server/main.tf
@@ -30,20 +30,28 @@ data "template_file" "app_data" {
   vars     = var.app_env_vars
 }
 
+
+# App
+data "aws_security_group" "app" {
+  vpc_id = data.aws_vpc.selected.id
+
+  filter {
+    name   = "group-name"
+    values = ["security-group-${local.name_suffix}-${var.app.name}-app"]
+  }
+}
+
+data "aws_subnet_ids" "private_subnets" {
+  vpc_id = data.aws_vpc.selected.id
+
+  filter {
+    name   = "tag:Tier"
+    values = ["private"]
+  }
+}
+
 locals {
   name_suffix = "${data.aws_region.current.name}-${substr(var.app.env, 0, 1)}-${var.app.id}"
-  app_sg_filters = [
-    {
-      name   = "group-name"
-      values = ["security-group-${local.name_suffix}-${var.app.name}-app"]
-    },
-  ]
-  app_subnet_filters = [
-    {
-      name   = "tag:Tier"
-      values = ["private"]
-    },
-  ]
   tags = {
     AppId       = var.app.id
     App         = var.app.name
@@ -52,17 +60,6 @@ locals {
     Environment = var.app.env
     #Time        = formatdate("YYYYMMDDhhmmss", timestamp())
   }
-}
-
-module "ec2_app" {
-  source                 = "../ec2"
-  app                    = merge(var.app, { role = local.tags["Role"] })
-  ami                    = var.image_id
-  instance_count         = lookup(var.scaling_capacity, "desired")
-  instance_type          = var.instance_type
-  security_group_filters = local.app_sg_filters
-  subnet_filters         = local.app_subnet_filters
-  user_data              = data.template_file.app_data.rendered
 }
 
 module "alb" {
@@ -119,12 +116,12 @@ module "asg" {
   lc_name         = "lc-${local.name_suffix}-${var.app.name}-app"
   image_id        = var.image_id
   instance_type   = var.instance_type
-  security_groups = module.ec2_app.sg_ids
+  security_groups = list(data.aws_security_group.app.id)
   user_data       = data.template_file.app_data.rendered
 
   # Auto scaling group
   asg_name                  = "asg-${local.name_suffix}-${var.app.name}-app"
-  vpc_zone_identifier       = module.ec2_app.subnets_ids
+  vpc_zone_identifier       = data.aws_subnet_ids.private_subnets.ids
   health_check_type         = "EC2"
   min_size                  = lookup(var.scaling_capacity, "min")
   max_size                  = lookup(var.scaling_capacity, "max")
@@ -133,10 +130,4 @@ module "asg" {
   target_group_arns         = module.alb.target_group_arns
 
   tags_as_map = local.tags
-}
-
-resource "aws_alb_target_group_attachment" "app" {
-  count            = length(module.ec2_app.ids)
-  target_group_arn = module.alb.target_group_arns[0]
-  target_id        = module.ec2_app.ids[count.index]
 }
