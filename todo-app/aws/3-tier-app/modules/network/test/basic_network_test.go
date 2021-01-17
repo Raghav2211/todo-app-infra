a6a927ea19f96e85b60e5309720039a03d8e522a
Raghav2211 <rghvj.22@gmail.com>
2021-01-17 16:26:39 +0000

    Add automated test cases for vpc module // initial commit

---
+++ todo-app/aws/3-tier-app/modules/network/test/basic_network_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestBasicNetworkModuleExample(t *testing.T) {
+	region := "us-west-2"
+	varFiles := make([]string, 1, 1)
+	varFiles[0] = "test/basic_network_test.tfvars.example"
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+		VarFiles:     varFiles,
+	})
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+	vpcId := terraform.Output(t, terraformOptions, "vpc_id")
+	subnets := aws.GetSubnetsForVpc(t, vpcId, region)
+	require.Equal(t, 6, len(subnets))
+}
