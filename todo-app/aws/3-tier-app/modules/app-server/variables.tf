30b4607ec9b839850b29fbf45f813c58610b8772
Ayashkant Baral <Ayashkant.Baral@rishabh.com>
2021-02-06 11:36:34 +0530

    asg module separated from app server module
    asg module separated from app server module

--- todo-app/aws/3-tier-app/modules/app-server/variables.tf
+++ todo-app/aws/3-tier-app/modules/app-server/variables.tf
@@ -35,38 +35,13 @@ variable "app_health" {
 
 
 # auto-scaling group & launch configuration
-variable "image_id" {
-  type        = string
-  description = "Application AMI id"
-  validation {
-    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
-    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
-  }
-}
+variable "image_id" {}
 
-variable "instance_type" {
-  type        = string
-  description = "App instance type"
-}
+variable "instance_type" {}
 
 
-variable "scaling_capacity" {
-  type        = map(string)
-  description = "Scaling paramter for EC2 Auto scaling group"
-  default = {
-    min     = 1
-    desired = 2
-    max     = 3
-  }
-}
+variable "scaling_capacity" {}
 
-variable "app_installer_tpl_path" {
-  type        = string
-  description = "File path to use app deployment"
-}
+variable "app_installer_tpl_path" {}
 
-variable "app_env_vars" {
-  type        = map
-  default     = {}
-  description = "App deployment enviornment variables"
-}
+variable "app_env_vars" {}
---
+++ todo-app/aws/3-tier-app/modules/asg/variables.tf
@@ -0,0 +1,62 @@
+variable "app" {
+  type = object(
+    {
+      id      = string
+      name    = string
+      version = string
+      env     = string
+    }
+  )
+}
+
+# auto-scaling group & launch configuration
+variable "image_id" {
+  type        = string
+  description = "Application AMI id"
+  validation {
+    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+  }
+}
+
+variable "instance_type" {
+  type        = string
+  description = "App instance type"
+}
+
+# auto-scaling group & launch configuration
+variable "image_id" {
+  type        = string
+  description = "Application AMI id"
+  validation {
+    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+  }
+}
+
+variable "instance_type" {
+  type        = string
+  description = "App instance type"
+}
+
+
+variable "scaling_capacity" {
+  type        = map(string)
+  description = "Scaling paramter for EC2 Auto scaling group"
+  default = {
+    min     = 1
+    desired = 2
+    max     = 3
+  }
+}
+
+variable "app_installer_tpl_path" {
+  type        = string
+  description = "File path to use app deployment"
+}
+
+variable "app_env_vars" {
+  type        = map
+  default     = {}
+  description = "App deployment enviornment variables"
+}
