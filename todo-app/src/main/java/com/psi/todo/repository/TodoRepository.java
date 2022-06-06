ce218803a072d05b42cc8268ac57f0eebe94eea1
Raghav2211 <rghvj.22@gmail.com>
2022-06-06 23:09:51 +0530

    remove code and inherit docker images from https://github.com/Raghav2211/spring-web-flux-todo-app.git

--- todo-app/src/main/java/com/psi/todo/repository/TodoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.psi.todo.repository;
-
-import com.psi.todo.entity.Todo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TodoRepository extends CrudRepository<Todo, Long> {}
