5cca9c12be6837bd2e6e9558e6e3f7085191670d
Raghav2211 <rghvj.22@gmail.com>
2022-06-15 20:06:16 +0530

    correct dir structure

--- aws/3-tier-app/lab/us-east-2/vpc/provider.tf
+++ aws/3-tier-app/lab/us-east-2/vpc/provider.tf
@@ -1,9 +1,9 @@
 terraform {
 
   backend "s3" {
-    bucket = "todo-state-lab"
-    key    = "network/vpc.tf"
-    region = "us-east-2"
+    bucket         = "todo-state-lab"
+    key            = "network/vpc.tf"
+    region         = "us-east-2"
     dynamodb_table = "terraform-state-lock"
   }
   required_version = "= 1.2.2"
--- aws/3-tier-app/modules/network/test/go.mod
+++
@@ -1,8 +0,0 @@
-module network_test
-
-go 1.15
-
-require (
-	github.com/gruntwork-io/terratest v0.31.4
-	github.com/stretchr/testify v1.7.0
-)
