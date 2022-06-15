942515756459da76936649cff4af50d94b481e70
Raghav2211 <rghvj.22@gmail.com>
2022-06-15 22:18:45 +0530

    correct packer spring todo AMI

--- aws/3-tier-app/packer/todo/scripts/installer.sh
+++ aws/3-tier-app/packer/todo/scripts/installer.sh
@@ -8,40 +8,39 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confd
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 sudo apt-get -y -qq install curl git openjdk-11-jdk maven apt-transport-https ca-certificates
 
-# Setup sudo to allow no-password sudo for "psi" group and adding "todo" user
-sudo groupadd -r psi
-sudo useradd -m -s /bin/bash todo
-sudo usermod -a -G psi todo
+# Setup sudo to allow no-password sudo for "todo" group and adding "springtodo" user
+sudo groupadd -r todo
+sudo useradd -m -s /bin/bash springtodo
+sudo usermod -a -G todo springtodo
 sudo cp /etc/sudoers /etc/sudoers.orig
-echo "todo  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/todo
+echo "todo  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/springtodo
 
 # Installing SSH key
-sudo mkdir -p /home/todo/.ssh
-sudo chmod 700 /home/todo/.ssh
-sudo cp /tmp/todo.pub /home/todo/.ssh/authorized_keys
-sudo chmod 600 /home/todo/.ssh/authorized_keys
-sudo chown -R todo /home/todo/.ssh
-sudo usermod --shell /bin/bash todo
+sudo mkdir -p /home/springtodo/.ssh
+sudo chmod 700 /home/springtodo/.ssh
+sudo cp /tmp/todo.pub /home/springtodo/.ssh/authorized_keys
+sudo chmod 600 /home/springtodo/.ssh/authorized_keys
+sudo chown -R springtodo /home/springtodo/.ssh
+sudo usermod --shell /bin/bash springtodo
 
 
 
 # Create JAVA_HOME for todo & download the todo-app from github
 
-sudo -H -i -u todo -- env bash << EOF
+sudo -H -i -u springtodo -- env bash << EOF
 whoami
-echo ~todo
-cd /home/todo
+echo ~springtodo
+cd /home/springtodo
 export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/
 export PATH=$PATH:$JAVA_HOME/bin
-git clone https://github.com/Raghav2211/psi-lab.git
-cd psi-lab/todo-app
-mvn clean package -DskipTests
+git clone https://github.com/Raghav2211/spring-web-flux-todo-app.git
+cd spring-web-flux-todo-app
+mvn clean package
 sudo mkdir /opt/todo
-sudo cp target/psi-todo-${APP_VERSION}.jar /opt/todo/app.jar
+sudo cp target/todo-${APP_VERSION}.jar /opt/todo/app.jar
 sudo cp /tmp/bootstrap.sh /opt/todo/bootstrap.sh
 sudo chmod 744 /opt/todo/bootstrap.sh
 sudo cp /tmp/app.service /etc/systemd/system/todo.service
-cd ../..
-rm -r psi-lab
+cd ..
+rm -r spring-web-flux-todo-app
 EOF
-
