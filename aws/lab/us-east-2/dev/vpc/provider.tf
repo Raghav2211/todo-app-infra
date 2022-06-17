6a5406db4a7913bb7c2427e73c7e20866b2bd197
Raghav2211 <rghvj.22@gmail.com>
2022-06-17 19:06:29 +0530

    fix bucket name in vpc fix output in tf-state module

--- aws/lab/us-east-2/dev/vpc/provider.tf
+++ aws/lab/us-east-2/dev/vpc/provider.tf
@@ -1,10 +1,12 @@
 terraform {
 
   backend "s3" {
-    bucket         = "tf-state-lab-dev"
+    bucket         = "todo-tf-state-lab"
     key            = "network/vpc.tf"
     region         = "us-east-2"
-    dynamodb_table = "tf-state-lab-dev"
+    encrypt        = true
+    kms_key_id     = "alias/todo-tf-state-key"
+    dynamodb_table = "todo-tf-state-lab"
   }
   required_version = "= 1.2.2"
 }
--- aws/modules/tf-state/outputs.tf
+++ aws/modules/tf-state/outputs.tf
@@ -1,7 +1,7 @@
 output "tf_state_bucket_arn" {
-  value = module.tf_state_bucket.s3_bucket_arn
+  value = module.todo-tf-state-bucket.s3_bucket_arn
 }
 
 output "tf_state_lock_table_id" {
-  value = aws_dynamodb_table.tf_state_lock.id
+  value = aws_dynamodb_table.todo-tf-state-lock.id
 }
