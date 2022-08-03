32bb3544a00e562ea656efffa7a20caaf3e1e1e3
Raghav2211 <rghvj.22@gmail.com>
2022-08-03 19:27:13 +0530

    add configuration endpoint in redis cluster output

---
+++ aws/lab/us-east-2/dev/elasticache/redis/todo/outputs.tf
@@ -0,0 +1,3 @@
+output "configuration_endpoint_address" {
+  value = module.dev_redis_cluster.configuration_endpoint_address
+}
---
+++ aws/modules/elasticache/redis/outputs.tf
@@ -0,0 +1,3 @@
+output "configuration_endpoint_address" {
+  value = aws_elasticache_replication_group.this.configuration_endpoint_address
+}
