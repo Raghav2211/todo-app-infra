8c0ff3e90d27f40dcc84cff6b1df4b960771ef5f
Raghav2211 <rghvj.22@gmail.com>
2022-08-04 13:07:47 +0530

    fix data source

--- aws/modules/apps/external-dns/data_sources.tf
+++ aws/modules/apps/external-dns/data_sources.tf
@@ -4,11 +4,11 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
 
   config = {
-    bucket         = "todo-tf-state-lab"
+    bucket         = "todo-tf-state-${var.app.account}"
     key            = "${var.app.account}/${data.aws_region.current.name}/${var.app.environment}/eks/${var.app.environment}.tf"
     region         = data.aws_region.current.name
     encrypt        = true
     kms_key_id     = "alias/todo-tf-state-key"
-    dynamodb_table = "todo-tf-state-lab"
+    dynamodb_table = "todo-tf-state-${var.app.account}"
   }
 }
