e25bcf5401bf1906de258efaa6380a8acc4bc2ab
Raghav2211 <rghvj.22@gmail.com>
2022-08-17 19:48:03 +0530

    refactor

--- k8s/deployment-validate.sh
+++ k8s/deployment-validate.sh
@@ -1,33 +1,16 @@
 # #!/bin/bash
-n=0
-while [ "$(kubectl get pods -l=app.kubernetes.io/name='config-server' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
-   sleep 5
-    n=$((n+1))
-    if [ "$n" -eq 5 ]; then
-          exit 1;
-    fi
-   echo "Waiting for config-server to be ready."
-done
-echo "config-server is ready"
 
-n=0
-while [ "$(kubectl get pods -l=app.kubernetes.io/name='edge-service' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
-   sleep 5
-    n=$((n+1))
-    if [ "$n" -eq 5 ]; then
-          exit 1;
-    fi
-   echo "Waiting for edge-service to be ready."
-done
-echo "edge-service is ready"
+services=('config-server' 'edge-service' 'todo')
 
-n=0
-while [ "$(kubectl get pods -l=app.kubernetes.io/name='todo' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
-   sleep 5
-    n=$((n+1))
-    if [ "$n" -eq 5 ]; then
-          exit 1;
-    fi
-   echo "Waiting for todo-app to be ready."
+for str in "${services[@]}"; do
+  n=0
+  while [ "$(kubectl get pods -l=app.kubernetes.io/name=$str -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+     sleep 5
+      n=$((n+1))
+      if [ "$n" -eq 5 ]; then
+            exit 1;
+      fi
+     echo "Waiting for $str to be ready."
+  done
+  echo "$str is ready"
 done
-echo "todo-app is ready"
