66b31a5f4755d059423330314827f4318d05e8c3
Raghav2211 <rghvj.22@gmail.com>
2022-08-17 19:30:52 +0530

    verify cluster step

---
+++ k8s/deployment-validate.sh
@@ -0,0 +1,33 @@
+# #!/bin/bash
+n=0
+while [ "$(kubectl get pods -l=app.kubernetes.io/instance='config-server' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+   sleep 5
+    n=$((n+1))
+    if [ "$n" -eq 5 ]; then
+          exit 1;
+    fi
+   echo "Waiting for config-server to be ready."
+done
+echo "config-server is ready"
+
+n=0
+while [ "$(kubectl get pods -l=app.kubernetes.io/instance='edge-service' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+   sleep 5
+    n=$((n+1))
+    if [ "$n" -eq 5 ]; then
+          exit 1;
+    fi
+   echo "Waiting for edge-service to be ready."
+done
+echo "edge-service is ready"
+
+n=0
+while [ "$(kubectl get pods -l=app.kubernetes.io/instance='todo' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+   sleep 5
+    n=$((n+1))
+    if [ "$n" -eq 5 ]; then
+          exit 1;
+    fi
+   echo "Waiting for todo-app to be ready."
+done
+echo "todo-app is ready"
