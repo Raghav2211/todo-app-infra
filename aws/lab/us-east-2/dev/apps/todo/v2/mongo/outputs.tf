6073c392389ed13589c260a6cd071d2bf01db205
Raghav2211 <rghvj.22@gmail.com>
2022-08-18 17:15:03 +0530

    add region in aws provider for todo/mongo

--- aws/lab/us-east-2/dev/apps/todo/v2/mongo/outputs.tf
+++ aws/lab/us-east-2/dev/apps/todo/v2/mongo/outputs.tf
@@ -1,15 +1,15 @@
 output "cluster_name" {
-  value = module.dev_mongo.cluster_name
+  value = module.todo_mongo.cluster_name
 }
 
 output "arn" {
-  value = module.dev_mongo.arn
+  value = module.todo_mongo.arn
 }
 
 output "writer_endpoint" {
-  value = module.dev_mongo.writer_endpoint
+  value = module.todo_mongo.writer_endpoint
 }
 
 output "reader_endpoint" {
-  value = module.dev_mongo.reader_endpoint
+  value = module.todo_mongo.reader_endpoint
 }
