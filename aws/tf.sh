5f96b56a189bcb42d332fcbaf6073c83875328d8
Raghav2211 <rghvj.22@gmail.com>
2022-08-22 16:47:41 +0530

    provide init/validate/plan for v2 modules

--- aws/tf.sh
+++ aws/tf.sh
@@ -1,36 +1,121 @@
 #!/bin/bash
 
-account=$1
-region=$2
-if [[ $region == "global" ]]; then
-  component=$3
-  cmd=$4
-else
-  environment=$3
-  component=$4
-  cmd=$5
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+fmt() {
+  find . \( -path '*/.terraform/*' \) -prune -false -o -type f -name '*.tf' | while read f
+    do
+      echo $f
+      terraform fmt $f
+
+    done
+}
+
+v2_buila_all() {
+  local account=$1
+  local region=$2
+  local environment=$3
+  module_config=$(jq '."'"$account"'"' todo-v2-tf-modules-config.json)
+  global_modules=$(jq -r '.global[]' <<<  $module_config) 
+  region_modules=$(jq -r '."'"$region"'"[]' <<<  $module_config) 
+  for global_module in $global_modules; do
+    tf lab global $global_module init -reconfigure
+    tf lab global $global_module validate
+    tf lab global $global_module plan
+  done
+  for region_module in $region_modules; do
+    if [ $( jq 'has("'"${region_module}"'")' <<< $module_config ) == "true" ]; then 
+      submodules=$( jq -r '."'"$region_module"'"[] | paths(scalars) as $p | [ ( [ $p[] | tostring ] | join("/") ) , (getpath($p)) ] | join("/")' <<< $module_config)
+      for submodule in $submodules; do
+        component=$region_module/${submodule///[[:digit:]]/}
+        echo "Applying for region :- ${region}"
+        tf $account $region $environment $component init -reconfigure
+        tf $account $region $environment $component validate
+        tf $account $region $environment $component plan
+      done
+    fi
+  done
+}
+
+
+tf() {
+  local account=$1
+  local region=$2
+  if [[ $region == "global" ]]; then
+    local component=$3
+    local cmd=$4
+    echo "\n🔧 Run command[$cmd] for account[$account] component[$component]"
+    if [[ $cmd == "plan" ]]; then
+      terraform -chdir=$account/global/$component $cmd -out $account-global-${component////-}.plan
+    elif [[ $cmd == "apply" ]]; then
+      terraform -chdir=$account/global/$component $cmd $account-global-${component////-}.plan
+    else
+      terraform -chdir=$account/global/$component $cmd
+    fi
+  else
+    local environment=$3
+    local component=$4
+    local cmd=$5
+    echo "\n🔧 Run command[$cmd] for for account[$account] region[$region] environment[$environment] component[$component]"
+    if [[ $cmd == "plan" ]]; then
+      terraform -chdir=$account/$region/$environment/$component $cmd -out $account-$region-$environment-${component////-}.plan
+    elif [[ $cmd == "apply" ]]; then
+        terraform -chdir=$account/$region/$environment/$component $cmd $account-$region-$environment-${component////-}.plan
+    else
+      terraform -chdir=$account/$region/$environment/$component $cmd
+    fi
+  fi
+}
+
+help() {
+    echo "Usage:    ${me} [OPTIONS] COMMAND"
+    echo ""
+    echo "Author:"
+    echo "   TODO APP INFRA Contributors - <$(git config --get remote.origin.url)>"
+    echo ""
+    echo "Options:"
+    echo " --debug, -D                                                            Enable debug mode"
+    echo " help, -h                                                               show help"
+    echo ""
+    echo "Commands:"
+    echo " v2-build-all <account> <aws-region> <environment>                      Build(init/validate/plan) all modules for todoV2"
+}
+
+
+if [ $# -lt 1 ]; then
+    help
 fi
 
-if [[ $region == "global" ]]; then
-  echo "\n🔧 $cmd for $account-global-$component"
 
-  if [[ $cmd == "plan" ]]; then
-    terraform -chdir=$account/global/$component plan -out $account-global-${component////-}.plan
-  elif [[ $cmd == "apply" ]]; then
-    terraform -chdir=$account/global/$component apply $account-global-${component////-}.plan
-  else
-    terraform -chdir=$account/global/$component $cmd
-  fi
+while test -n "$1"; do
+   case "$1" in
+       -h|help)
+          help
+          shift
+          ;;
+       fmt)
+         fmt
+         shift
+         ;;
+       v2-build-all)
+         if [[ $# -gt 4 ]]; then
+          echo "Too many args";
+          help
+          exit 1;
+         elif [[ $# -lt 4 ]]; then
+          echo "Less args";
+          help
+          exit 1;	   
+         fi   
+         v2_buila_all $2 $3 $4
+         exit $?
+         ;;
+       *)
+          echo "Unrecognized option : ${1}"
+          help
+          exit 1;
+   esac
+done
 
-else
-  echo "\n🔧 $cmd for $account-$region-$environment-$component"
 
-  if [[ $cmd == "plan" ]]; then
-    terraform -chdir=$account/$region/$environment/$component plan -out $account-$region-$environment-${component////-}.plan
-  elif [[ $cmd == "apply" ]]; then
-      terraform -chdir=$account/$region/$environment/$component apply $account-$region-$environment-${component////-}.plan
-  else
-    terraform -chdir=$account/$region/$environment/$component $cmd
-  fi
 
-fi
--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
